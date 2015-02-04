3475020f1a597df85ac39bf9debf21973436904f
Max Kerscher-Santelli <mkersche@ucsc.edu>
2015-02-04 13:51:20 -0800

    more shit

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 /**
  * @author Max Kerscher-Santelli
  */
+var bunny;
+var stage;
+var renderer;
+
 function initGame(){
 	// create an new instance of a pixi stage
-	var stage = new PIXI.Stage(0xFFFFFF);
+	stage = new PIXI.Stage(0xFFFFFF);
 
 	// create a renderer instance
-	var renderer = new PIXI.WebGLRenderer(400, 300);//autoDetectRenderer(400, 300);
+	renderer = new PIXI.WebGLRenderer(400, 300);//autoDetectRenderer(400, 300);
 
 	// add the renderer view element to the DOM
 	document.body.appendChild(renderer.view);
 
-	requestAnimFrame( animate );
+	requestAnimFrame( update );
 
 	// create a texture from an image path
 	var texture = PIXI.Texture.fromImage("images/Ultramarineblue.jpg");
 	// create a new Sprite using the texture
-	var bunny = new PIXI.Sprite(texture);
+	bunny = new PIXI.Sprite(texture);
 
 	// center the sprites anchor point
 	bunny.anchor.x = 0.5;
@@ -27,23 +31,27 @@ function initGame(){
 	bunny.position.y = 150;
 
 	stage.addChild(bunny);
-
-	function animate() {
-
-	    requestAnimFrame( animate );
-
-	    // just for fun, lets rotate mr rabbit a little
-	    bunny.rotation += 0.1;
-
-	    // render the stage
-	    renderer.render(stage);
-	}
 }
 
 function update(){
+	requestAnimFrame( update );
+
+	// just for fun, lets rotate mr rabbit a little
+	bunny.rotation += 0.1;
+
+	// render the stage
+	renderer.render(stage);
+	console.log("update");
 	
+	if(gameOver()){
+		newGame();
+	}
 }
 
 function newGame(){
 	
+}
+
+function gameOver(){
+	return false;
 }
